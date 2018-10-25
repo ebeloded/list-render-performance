f7e08e6b679f6b383ef5220343d75d48306d5cd8
Evgenij Beloded <belodedevgenij@gmail.com>
2018-10-25 14:45:07 +0700

    Update number of generated items

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './Components/App'
 import generateItems from './generateItems';
 
-const items = generateItems(15)
+const items = generateItems(25)
 
 ReactDOM.render(<App items={items} />, document.getElementById('app'))
